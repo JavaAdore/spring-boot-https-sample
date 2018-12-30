a998c77bae4f4335567e59f037d1b61e253fb35e
Java Adore <java.adore@gmail.com>
2018-12-31 00:36:04 +0400

    sample implementation of https in spring boot

---
+++ src/main/java/com/sample/ssl/httpssample/HttpsSampleApplication.java
@@ -0,0 +1,14 @@
+package com.sample.ssl.httpssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsSampleApplication.class, args);
+	}
+
+}
+
